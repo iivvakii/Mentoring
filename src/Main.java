aa045e576737f60f31d0565f54d9784c7c0fa885
iivvaki <ivan.stefaniuk.kn.2021@lpnu.ua>
2024-03-19 17:54:54 +0200

    task 4(with custom exceptions)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import exeptions.AgeExeption;
+import exeptions.NameExeption;
 import task3.MyDateComparator;
 import task3.Person;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -15,12 +19,16 @@ public class Main {
         bufferedReader.readLine();
         while((current = bufferedReader.readLine()) != null){
             String[] infoPerson = current.split(" ");
+
             try {
-                people.add(new Person(infoPerson[0], infoPerson[1], infoPerson[24]));
+                checkAge(infoPerson[1]);
+            } catch (AgeExeption e) {
+                throw new RuntimeException(e);
             }
-            catch (ArrayIndexOutOfBoundsException e){ //unchecked exeption
-                System.out.println(e);
-            }
+            checkName(infoPerson[0]);
+
+            people.add(new Person(infoPerson[0], infoPerson[1], infoPerson[2]));
+
         }
 
         for (Person person : people){
@@ -29,5 +37,26 @@ public class Main {
     }
 
 
+    public static void checkAge(String date) throws AgeExeption {
+        String[] dates = date.split("\\.");
+        LocalDate birthdate = LocalDate.of(Integer.parseInt(dates[2]), Integer.parseInt(dates[1]), Integer.parseInt(dates[0]));
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthdate, currentDate);
 
+        // Print Age
+//        System.out.println("Age: " + period.getYears() + " years");
+        if(period.getYears() < 18){
+            throw new AgeExeption("You must be 18+.");
+        }
+        else{
+            System.out.println("You have 18.");
+        }
+    }
+
+
+    public static void checkName(String name){
+        if(name.length() < 4){
+            throw new NameExeption("This name does not have enough letters");
+        }
+    }
 }
---
+++ src/exeptions/AgeExeption.java
@@ -0,0 +1,7 @@
+package exeptions;
+
+public class AgeExeption extends Exception{
+    public AgeExeption(String message) {
+        super(message);
+    }
+}
