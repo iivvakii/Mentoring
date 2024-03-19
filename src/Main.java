9639f17ea09c1873f49013d5f8c7d2db0e09e93c
iivvaki <ivan.stefaniuk.kn.2021@lpnu.ua>
2024-03-19 14:19:48 +0200

    task 4

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,19 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { //checked exeption
         Set<Person> people = new TreeSet<>(new MyDateComparator());
         BufferedReader bufferedReader = new BufferedReader(new FileReader("C:/Users/User/OneDrive/Робочий стіл/people.txt"));
         String current;
         bufferedReader.readLine();
         while((current = bufferedReader.readLine()) != null){
             String[] infoPerson = current.split(" ");
-            people.add(new Person(infoPerson[0], infoPerson[1], infoPerson[2]));
+            try {
+                people.add(new Person(infoPerson[0], infoPerson[1], infoPerson[24]));
+            }
+            catch (ArrayIndexOutOfBoundsException e){ //unchecked exeption
+                System.out.println(e);
+            }
         }
 
         for (Person person : people){
